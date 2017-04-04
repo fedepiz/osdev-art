1054ef942ab5adbd9d37107c4f149f0c9616617a
federico <pizzutifederico27@gmail.com>
2017-04-04 17:43:50 +0100

    Added in bits and pieces needed for higher half kernelling

--- src/arch/boot.s
+++ src/arch/boot.s
@@ -38,6 +38,29 @@ heap_top:
 resb 16777216; 16 MiB
 heap_bottom:
 
+; This is the virtual base address of kernel space. It must be used to convert virtual
+; addresses into physical addresses until paging is enabled. Note that this is not
+; the virtual address where the kernel image itself is loaded -- just the amount that must
+; be subtracted from a virtual address to get a physical address.
+KERNEL_VIRTUAL_BASE equ 0xC0000000                  ; 3GB
+KERNEL_PAGE_NUMBER equ (KERNEL_VIRTUAL_BASE >> 22)  ; Page directory index of kernel's 4MB PTE.
+section .data
+align 0x1000
+BootPageDirectory:
+    ; This page directory entry identity-maps the first 4MB of the 32-bit physical address space.
+    ; All bits are clear except the following:
+    ; bit 7: PS The kernel page is 4MB.
+    ; bit 1: RW The kernel page is read/write.
+    ; bit 0: P  The kernel page is present.
+    ; This entry must be here -- otherwise the kernel will crash immediately after paging is
+    ; enabled because it can't fetch the next instruction! It's ok to unmap this page later.
+    dd 0x00000083
+    times (KERNEL_PAGE_NUMBER - 1) dd 0                 ; Pages before kernel space.
+    ; This page directory entry defines a 4MB page containing the kernel.
+    dd 0x00000083
+    times (1024 - KERNEL_PAGE_NUMBER - 1) dd 0  ; Pages after the kernel image.
+ 
+
 ; The linker script specifies _start as the entry point to the kernel and the
 ; bootloader will jump to this position once the kernel has been loaded. It
 ; doesn't make sense to return from this function as the bootloader is gone.
@@ -77,9 +100,6 @@ _start:
 	; aligned above and we've since pushed a multiple of 16 bytes to the
 	; stack since (pushed 0 bytes so far) and the alignment is thus
 	; preserved and the call is well defined.
-  ; print `OK` to screen
-  ;mov dword [0xb8000], 0x2f4b2f4f
-  ;hlt
   extern kernel_main
 	call kernel_main
 
